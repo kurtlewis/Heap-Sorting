ee0cfef5bf9afa803a4585e57ce45cf6b1cc769e
Kurt Lewis <kurt.jlewis15@gmail.com>
2016-02-10 20:10:17 -0500

    I have the basic tree done, but its currently pushed to the right, and the top row is entirely wrong. Making this commit to save changes before pushing forward on trying to fix it.

--- Heap Sorting/Source.cpp
+++ Heap Sorting/Source.cpp
@@ -13,17 +13,15 @@ void printArray(int arraySize, int array[]);
 
 int main()
 {
-	bool min = true;
-	const int arraySize = 20;
-	int array[] = { 10, 4, 9, 2, 6, 7, 1, 0, 3, 5, 8, 12, 18, 16, 11, 14, 17, 13, 15, 19};
-	//int array[] = ;
-	/*for (int i = 0; i < arraySize; i++)
+	bool min = false;
+	const int arraySize = 15;
+	//int array[] = { 10, 4, 9, 2, 6, 7, 1, 0, 3, 5, 8, 12, 18, 16, 11, 14, 17, 13, 15, 19};
+	int array[arraySize];
+	srand((unsigned)time(0));
+	for (int i = 0; i < arraySize; i++)
 	{
-		time_t seconds;
-		time(&seconds);
-		srand((unsigned int)seconds);
-		array[i] = rand() % arraySize + 1;
-	}*/
+		array[i] = rand() % arraySize + 10;
+	}
 	printArray(arraySize, array);
 	std::cin.ignore();
 	system("cls");
@@ -114,37 +112,63 @@ int sortHeapStep(int array[], int arraySize, bool min)
 void printArray(int arraySize, int array[])
 {
 	//for when I implement the arrows between rows of numbers
-	//int rows = ceil(log2((double)arraySize)) * 2 - 1;
+	int formatRows = ceil(log2((double)arraySize)) * 2 - 1;
 	//temporary for no lines between numbers
 	int rows = ceil(log2((double)arraySize));
 	//The size of a cell is the number of digits in the largest number + 2
-	int cellSize = ceil(log((double)arraySize)) + 2;
-	int cellsInBottom = pow(2, (rows / 2) - 1);
+	//int cellSize = ceil(log((double)arraySize)) + 2;
+	// temporary cellSize because the above formula doesn't work.
+	int cellSize = 4;
+	int cellsInBottom = pow(2, rows - 1);
+	int index = 0;
 
-	for (int row = 0; row < rows; row++)
+	for (int row = 0; row < formatRows; row++)
 	{
-		for (int i = pow(2, row) - 1; i < 2 * pow(2, row) - 1; i++)
+		/*for (int i = pow(2, row) - 1; i < 2 * pow(2, row) - 1; i++)
 		{
-			std::cout << array[i] << " ";
+			std::cout << " " << array[i] << " ";
 			if (i > arraySize - 2)
 			{
 				break;
 			}
-		}
-		/*for (int cell = 0; cell < cellsInBottom; cell++)
-		{
-			if (false) // its a cell I want
-			{
-
-			}
-			else
-			{
-				for (int i = 0; i < cellSize; i++)
-				{
-					std::cout << " ";
-				}
-			}
 		}*/
+		
+		if (row % 2 == 0)
+		{
+				// even rows are for numbers
+				int pos = 1;
+				for (int cell = 0; cell < cellsInBottom; cell++)
+				{
+					int temp = ((cellsInBottom / (pow(2, ceil((row + 1) / 2)))) * pos) - 1;
+					if (cell == temp) // its a cell I want
+					{
+						//commented out until the correct cellSize formula is found
+						/*for (int i = 0; i < cellSize - ceil(log((double)array[index])); i++)
+						{
+							std::cout << " ";
+						}*/
+						std::cout << " " << array[index] << " ";
+						index++;
+						pos++;
+					}
+					else
+					{
+						//temporary code to make debugging easier
+						std::cout << "    ";
+						/*for (int i = 0; i < cellSize; i++)
+						{
+							std::cout << " ";
+						}*/
+					}
+				}
+		}
+		else
+		{
+			// odd rows are for formatting
+			std::cout << std::endl;
+		}
+
+		
 		std::cout << std::endl;
 	}
 }
